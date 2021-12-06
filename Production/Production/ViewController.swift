59ad5c6624a64d6efa58161c68f0445be2de4eef
tdrk18 <s18.todoroki@gmail.com>
2021-12-06 23:24:28 +0900

    Add production project

---
+++ Production/Production/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Production
+//
+//  Created by shota.todoroki on 2021/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
