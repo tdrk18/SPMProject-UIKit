bf2be371dee6fe3c29e6e7c4a7823482bfe5e244
tdrk18 <s18.todoroki@gmail.com>
2021-12-06 23:29:25 +0900

    Add staging project

---
+++ Staging/Staging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Staging
+//
+//  Created by shota.todoroki on 2021/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
